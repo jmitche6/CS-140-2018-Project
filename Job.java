0e912c874fe9c3e3791e3af1162afef84f3fd45b
JohnMitchell493 <33502053+JohnMitchell493@users.noreply.github.com>
2018-04-26 17:41:57 -0400

    Job.java
    created Job.java.  This class is given in the prompt

---
+++ Job.java
@@ -0,0 +1,54 @@
+package project;
+import projectview.States;
+
+public class Job {
+	private int startcodeIndex;
+	private int codeSize;
+	private int startmemoryIndex;
+	private int currentIP;
+	private int currentAcc;
+	private States currentState;
+	
+	public States getCurrentState() {
+		return currentState;
+	}
+	public void setCurrentState(States currentState) {
+		this.currentState = currentState;
+	}
+	public int getCurrentAcc() {
+		return currentAcc;
+	}
+	public void setCurrentAcc(int currentAccumulator) {
+		this.currentAcc = currentAccumulator;
+	}
+	public int getStartcodeIndex() {
+		return startcodeIndex;
+	}
+	public void setStartcodeIndex(int startcodeIndex) {
+		this.startcodeIndex = startcodeIndex;
+	}
+	public int getCodeSize() {
+		return codeSize;
+	}
+	public void setCodeSize(int codeSize) {
+		this.codeSize = codeSize;
+	}
+	public int getStartmemoryIndex() {
+		return startmemoryIndex;
+	}
+	public void setStartmemoryIndex(int startmemoryIndex) {
+		this.startmemoryIndex = startmemoryIndex;
+	}
+	public int getCurrentIP() {
+		return currentIP;
+	}
+	public void setCurrentIP(int currentIP) {
+		this.currentIP = currentIP;
+	}
+	public void reset() {
+		codeSize = 0;
+		currentState = States.NOTHING_LOADED;
+		currentAcc = 0;
+		currentIP = startcodeIndex;
+	}
+}
