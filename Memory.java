99443fbd7690978852e8764cc6bd7f54969dd364
CBanvard <33142678+CBanvard@users.noreply.github.com>
2018-04-30 13:50:40 -0400

    Memory Update

--- Memory.java
+++ Memory.java
@@ -2,6 +2,24 @@ package project;
 
 public class Memory {
 	public static int DATA_SIZE = 2048;
-	private int[] data = new int[DATA_SIZE];	
+	private int[] data = new int[DATA_SIZE];
+	public static final int CODE_MAX = 2048;
+	private int[] code = new int[CODE_MAX];
+	int[] getCode() {
+		return code;
+	}
+	public int getOp(int i) {
+		return code[2*i];
+	}
+	public int getArg(int i) {
+		return code[2*i + 1];
+	}
+	public void clear(int start, int end) {
+		for(int i = start; i < end; i++) {
+			code[2*i]=0;
+			code[2*i+1]=0;
+		}
+	}
+
 
 }
