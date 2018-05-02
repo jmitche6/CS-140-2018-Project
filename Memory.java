87dc998b014ec91a1411262754b7cda18bbb7448
JohnMitchell493 <33502053+JohnMitchell493@users.noreply.github.com>
2018-05-02 14:16:29 -0400

    added part 2 changes
    added the changes to Memory from final project part 2, namely the int changedIndex, the methods setData, clearData, and getChangedIndex

--- Memory.java
+++ Memory.java
@@ -5,6 +5,8 @@ public class Memory {
 	private int[] data = new int[DATA_SIZE];
 	public static final int CODE_MAX = 2048;
 	private int[] code = new int[CODE_MAX];
+	private int changedIndex = -1;
+	
 	int[] getCode() {
 		return code;
 	}
@@ -14,16 +16,27 @@ public class Memory {
 	public int getArg(int i) {
 		return code[2*i + 1];
 	}
+	public int getChangedIndex() {
+		return changedIndex;
+	}
 	public void clear(int start, int end) {
 		for(int i = start; i < end; i++) {
 			code[2*i]=0;
 			code[2*i+1]=0;
 		}
 	}
+	public void clearData(int start, int end) {
+		changedIndex = -1;
+		for(int i = 0; start <= i < end; i++) {
+			data[i] = 0;
+		}
+	}
 	public void setCode(int index, int op, int arg) {
 		code[2*index] = op;
 		code[2*index+1] = arg;
 	}
-
+	public void setData(int index, int value) {
+		changedIndex = index;
+	}
 
 }
